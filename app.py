3a99c811506a71262f03f60053e36d2e6870536f
Steven Christe <steven.d.christe@nasa.gov>
2017-01-07 15:12:25 -0500

    added time stamp

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 import flask
 from flask import render_template
 import sunpy
+import datetime
 
 app = flask.Flask(__name__)
 
 @app.route('/')
 def index():
     text = sunpy.__version__
-    return render_template('index.html', text=text)
+    current_time = str(datetime.datetime.now())
+    return render_template('index.html', text=text, 
+current_time=current_time)
 
 if __name__ == '__main__':
     print(__doc__)
