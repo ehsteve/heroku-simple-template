caf2ae862bf9755c7e11ee0479907bb338e7ae10
Steven Christe <steven.d.christe@nasa.gov>
2017-01-08 06:23:10 -0500

    test download data

--- app.py
+++ app.py
@@ -3,11 +3,18 @@ from flask import render_template
 import sunpy
 import datetime
 
+import sunpy.lightcurve as lc
+from sunpy.time import TimeRange, parse_time
+
+DEFAULT_TR = TimeRange(['2011-06-07 00:00', '2011-06-07 12:00'])
+goes = lc.GOESLightCurve.create(DEFAULT_TR)
+
+
 app = flask.Flask(__name__)
 
 @app.route('/')
 def index():
-    text = sunpy.__version__
+    text = sunpy.__version__ + '\n' + goes.data.describe()
     current_time = str(datetime.datetime.now())
     return render_template('index.html', text=text, 
 current_time=current_time)
